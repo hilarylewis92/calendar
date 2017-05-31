298fdc0e94ce8558ca41544190e7cd7131bd3ab0
Hilary Lewis <hilarylewis92@gmail.com>
2017-05-31 12:53:01 -0600

    Add calendar

---
+++ src/components/Calendar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import CalendarComponent from 'react-day-calendar';
+
+class Calendar extends Component {
+  
+  render() {
+    return (
+      <div className="Calendar">
+        <CalendarComponent start={9} end={21} events={[
+            {start: 30, end: 150, title: 'Event 1', location: 'Barcelona'},
+            {start: 540, end: 600, title: 'Event 2', location: 'Munich'},
+            {start: 560, end: 620, title: 'Event 3', location: 'Berlin'},
+            {start: 610, end: 670, title: 'Event 4', location: 'London'}
+        ]}/>
+      </div>
+    );
+  }
+}
+
+export default Calendar;
